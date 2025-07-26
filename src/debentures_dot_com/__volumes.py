6a1dee233708acefbcd9ca2326f18f1ec65e1342
gtazevedo <guilherme.tt.azevedo@gmail.com>
2025-07-26 13:05:14 -0300

    Moved dev.ipynb to tests and marked marked volume as internal as its incomplete

---
+++ src/debentures_dot_com/__volumes.py
@@ -0,0 +1,9 @@
+
+volume/volumeporperiodo_e.asp?op_exc=False&emissao=0&dt_ini=01%2F07%2F2025&dt_fim=23%2F07%2F2025&ICVM=&moeda=1
+volume/volumeporperiodo_e.asp?op_exc=Nada&emissao=2&dt_ini=01%2F07%2F2024&dt_fim=23%2F07%2F2025&ICVM=&moeda=1
+
+/volume/volumeporgarantia-especie_d.asp?pMes_Ini=05&pAno_Ini=2025&pMes_Fim=07&pAno_Fim=2025&moeda=1&pConsol=1&op_exc=Nada&ICVM=%20NULL
+volume/volumeporgarantia-especie_d.asp?pMes_Ini=5&pAno_Ini=2025&pMes_Fim=5&pAno_Fim=2025&pConsol=0&moeda=1&op_exc=Nada&ICVM=%20NULL
+volume/volumeporgarantia-especie_de.asp?pMes_Ini=05&pAno_Ini=2025&pMes_Fim=07&pAno_Fim=2025&moeda=1&pConsol=1&op_exc=Nada&ICVM
+
+https://www.debentures.com.br/exploreosnd/consultaadados/volume/volumeporgarantia-especie_de.asp?pMes_Ini=05&pAno_Ini=2025&pMes_Fim=07&pAno_Fim=2025&moeda=1&pConsol=1&op_exc=Nada&ICVM=%20NULL&ICVM=NULL
